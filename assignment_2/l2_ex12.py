69cc9bc164e6c577a12da6368825ff0b8888ea00
xncvch2 <besscorbitt159@gmail.com>
2019-02-17 21:57:07 +0700

    Update l2_ex12.py

--- assignment_2/l2_ex12.py
+++ assignment_2/l2_ex12.py
@@ -10,18 +10,19 @@ def sed(command, s):
 			string1 = string1 + i
 		elif control == 2:
 			string2 = string2 + i
-	string_out = ''
+	string_out = s
 	if command[0] == 's':
-		pass
-
+		string_out = string_out.replace(string1, string2)
 
 	elif command[0] == 'y':
-		for i in s:
-			character = i
-			for j in range(len(string1)):
-				if character == string1[j]:
-					character = string2[j]
-			string_out = string_out + character
+		for i in range(len(string1)):
+			string_out = string_out.replace(string1[i], string2[i])
+		# for i in s:
+		# 	character = i
+		# 	for j in range(len(string1)):
+		# 		if character == string1[j]:
+		# 			character = string2[j]
+		# 	string_out = string_out + character
 	return string_out
 
 print(sed('y/aeo/430/', 'hello mr wayne'))
